f22837b0a204b3df2338726814c55df798f156ab
Lucas Oliveira <85700941+L-H-O@users.noreply.github.com>
2022-11-06 13:09:02 -0300

    Update Open Macro.py

--- Macroeconomia/Open Macro.py
+++ Macroeconomia/Open Macro.py
@@ -7,3 +7,22 @@ Estudos Macro Aberta
 @author: lider
 """
 
+import numpy as np
+import pandas as pd
+from pandas_datareader import data
+from datetime import datetime as date
+
+#%%
+
+def exchange():
+
+    ''' Extrai a taxa de câmbio com a precisão de duas casas decimais '''
+    
+    exchange_rate =  round(data.DataReader('BRL=X', 'yahoo')['Adj Close']\
+                    [pd.to_datetime(date.today()).strftime("%Y/%m/%d")], 2 )
+
+    return exchange_rate
+
+exchange_rate = exchange()
+    
+
