33eaabe8e2fc612863bf76ea5a4164679a570709
Lucas Oliveira <85700941+L-H-O@users.noreply.github.com>
2022-11-06 13:09:54 -0300

    Update Game Theory.py

--- Microeconomia/Game Theory.py
+++ Microeconomia/Game Theory.py
@@ -7,3 +7,19 @@ Estudos Teoria dos Jogos
 @author: lider
 """
 
+import numpy as np
+
+#%% Batalha dos Sexos
+
+def game(stratman, startwom):
+    
+    ''' Tomamos de início que o homem faz a primeira decisão,
+        seguido pela mulher '''
+    
+    utils = np.array([[0,1], [1,0],
+         [1,2], [0,0]]).reshape(2,4)
+    
+    if stratman == 0:
+        
+        
+
