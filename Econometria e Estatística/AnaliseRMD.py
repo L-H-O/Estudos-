dbed07611ec6d68d56b064ed954f6ae1026f3205
Lucas Oliveira <85700941+L-H-O@users.noreply.github.com>
2023-01-29 15:29:22 -0300

    Update AnaliseRMD.py

--- Econometria e Estatística/AnaliseRMD.py
+++ Econometria e Estatística/AnaliseRMD.py
@@ -9,7 +9,7 @@ import sys; sys.path.append(r"C:\Users\lider\Programming\Scrapings")
 import pandas as pd
 #import os
 import statsmodels.api as sm
-import re
+import dateparser
 
 from statsmodels.tsa.x13 import x13_arima_analysis as arima
 from RMD import RMD
@@ -35,6 +35,10 @@ def Reserves(df):
 df = RMD(1.2)
 
 dates = df.iloc[3,:]
+
+ndates = dates.apply(lambda x : dateparser.parse(x, languages = ['pt'], date_formats = ("%b")))
+
+dates_new = pd.to_datetime(dates, format = "%b/%y")
 
 
 aux = {'Jan':'Jan', 'Fev':'Feb', 'Mar':'Mar', 'Abr':'Apr',
@@ -93,7 +97,7 @@ def PMDPMFi(df):
     
     return df   
 
-# Composição da DPF
+# Composição da DPF, DPMFi e DPFe
 
 def compDPF(df):
     
@@ -104,36 +108,69 @@ def compDPF(df):
     
     return df
 
-# Composição da DPMFi
+# Detentores da DPMFi
 
-def compDPMFi(df):
+def ownersDPMFi(df):
     
     df = df[3:]
     df.columns = df.iloc[0,:]
-    df = df.iloc[2:]
-    df.index.name = 'Date'
+    df = df.iloc[1:]
+    df.index.name = "Date"
     
     return df
 
-# Composição da DPFe
+# Participação da DPMFi
 
-def compDPFe(df):
+def partDPMFi(df):
     
-    df = df[3:]
-    df.columns = df.iloc[0,:]
-    df = df.iloc[2:]
-    df.index.name = 'Date'
+    lft = df.loc["LFT":"LTN",:]
+    lft = lft[2:]
+    lft.columns = lft.iloc[0,:]
+    lft = lft[1:].dropna()
+    lft.index = pd.to_datetime(lft.index.where(~lft.index.isin(['mar/18*','abr/18*']), ['2018-04-01','2018-03-01']))
+    lft.index.name = 'Date'
     
-    return df
+    ltn = df.loc["LTN":"NTN-B",:]
+    ltn = ltn[2:]
+    ltn.columns = ltn.iloc[0,:]
+    ltn = ltn[1:].dropna()
+    ltn.index = pd.to_datetime(ltn.index.where(~ltn.index.isin(['mar/18*','abr/18*']), ['2018-04-01','2018-03-01']))
+    ltn.index.name = 'Date'
+  
+    ntnb = df.loc["NTN-B":"NTN-F",:]
+    ntnb = ntnb[2:]
+    ntnb.columns = ntnb.iloc[0,:]
+    ntnb = ntnb[1:].dropna()
+    ntnb.index = pd.to_datetime(ntnb.index.where(~ntnb.index.isin(['mar/18*','abr/18*']), ['2018-04-01','2018-03-01']))
+    ntnb.index.name = 'Date'
+    
+    ntnf = df.loc["NTN-F":"OUTROS",:]
+    ntnf = ntnf[2:]
+    ntnf.columns = ntnf.iloc[0,:]
+    ntnf = ntnf[1:].dropna()
+    ntnf.index = pd.to_datetime(ntnf.index.where(~ntnf.index.isin(['mar/18*','abr/18*']), ['2018-04-01','2018-03-01']))
+    ntnf.index.name = 'Date'
+    
+    others = df.loc["OUTROS":,:]    
+    others = others[2:]
+    others.columns = others.iloc[0,:]
+    others = others[1:].dropna()
+    others.index = pd.to_datetime(others.index.where(~others.index.isin(['mar/18*','abr/18*']), ['2018-04-01','2018-03-01']))
+    others.index.name = 'Date'
+       
+    return lft, ltn, ntnb, ntnf, others
 
 #%% Análise
 
-sheet = 3.9
+sheet = 6.1
 data = Reserves(RMD(sheet))
 
 dpf, dpmfi, dpfe = Vencimentos(RMD(sheet))
 
 dfpm = PMDPMFi(RMD(sheet))
 
-compdpf, compdpmfi, compdpfe = compDPF(RMD(2.4)), compDPMFi(RMD(2.5)), compDPFe(RMD(2.6))
+compdpf, compdpmfi, compdpfe = compDPF(RMD(2.4)), compDPF(RMD(2.5)), compDPF(RMD(2.6))
+
+lft, ltn, ntnb, ntnf, others = partDPMFi(RMD(2.8))
+
 
